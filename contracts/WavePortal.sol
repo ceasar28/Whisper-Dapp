0bac383bc4f1bb7b8e6b655c2f1d178902d5fbf6
Ekete Emmanuel <eketeeu@gmail.com>
2023-02-15 16:05:12 +0100

    added a struct for allowing sender send message

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -9,12 +9,22 @@ contract WavePortal {
     // a state varaiable
     uint256 totalWaves;
 
-    // array to store senders arrays
-    address[] senders;
+    event NewWave( address indexed from, uint256 timestamp, string message);
+
+    // a struct to store our wave
+    struct Wave {
+        address waver; // the address of who waved.
+        string message; // the message the user sent
+        uint256 timestamp; // the timestamp when the user waver
+    }
+
+    // an array of structs, which holds all the wave anyone ever send to me!
+
+    Wave[] waves;
 
     // this constructor runs on the first instance of the smart contract
     constructor(){
-        console.log("Yo yo, I am a contract and I am smart");
+        console.log("Whisper Smart contract");
     }
 
     /**
@@ -22,10 +32,12 @@ contract WavePortal {
 
     msg.sender is the wallet address of the person who called the function
      */ 
-    function wave() public {
+    function wave(string memory _message) public {
         totalWaves += 1;
-        senders.push(msg.sender);
-        console.log("%s has waved!", msg.sender);
+        waves.push(Wave(msg.sender,_message, block.timestamp));
+        console.log("%s whispered w/ message %s!", msg.sender);
+
+        emit NewWave(msg.sender,block.timestamp, _message);
     }
 
     // a view function that gets the value of totalwaves
@@ -34,8 +46,8 @@ contract WavePortal {
         return totalWaves;
     }
 
-    // function to get all senders
-    function getSenders()public view returns (address[] memory) {
-        return senders;
+    // function to get all waves struct from the array
+    function getAllWaves()public view returns (Wave[] memory) {
+        return waves;
     }
 }
